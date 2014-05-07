c3990e92a7fdec644727c1c51bfc3c9c63dc4932
Russ93 <russ93@fullsail.edu>
2014-05-07 19:58:10 -0400

    fixed the mailing feature

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
 <?php
 if(isset($_POST['submit'])) {
 	$msg = "<style>*{font-family:sans-serif;}</style>
-	Name: $_POST['name']<br />
-    Email: $_POST['email']<br />
-    Company: $_POST['company']<br />
-    <h3>Header: $_POST['header']</h3><br />
-	<p>Msg: $_POST['comment']</p>";
+	Name: ".$_POST['name']."<br />
+    Email: ".$_POST['email']."<br />
+    Company: ".$_POST['company']."<br />
+    <h3>Header: ".$_POST['header']."</h3><br />
+	<p>Msg: ".$_POST['comment']."</p>";
 
-	$subject = "Site Message from $_POST['email']";
+	$subject = "Site Message from ".$_POST['email'];
 	mail('russellschlup@gmail.com', $subject, $msg );
 	header('location: /');
 
